7b56e55fbf678817ad07818cbdb14a3c3656db1c
Aileen Alvarado <monsealvarado775@gmail.com>
2022-11-19 12:31:30 -0600

    first commit

---
+++ ingles/src/test/java/com/ingles/InglesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InglesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
